02ceb7d602996df5a1fffcb88a558764e1aca6cf
Stephane Jourdan <stephane.jourdan@cloudskiff.com>
2021-03-16 18:40:29 +0100

    quickly show the version issue with terraform 0.11

---
+++ demo-versions/providers.tf
@@ -0,0 +1,8 @@
+// terraform {
+//   required_version = "= 0.11.14"
+
+//   required_providers {
+//     aws    = "= 3.0.0"
+//     random = "= 3.0.0"
+//   }
+// }
---
+++ demo-versions/s3.tf
@@ -0,0 +1,5 @@
+resource "aws_s3_bucket" "demo" {
+  bucket = "demo-lkjdsf"
+  acl    = "private"
+}
+
